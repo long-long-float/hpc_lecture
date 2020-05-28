e8e1efccf046e2e6b37d53248974f7311f35fc34
long-long-float <niinikazuki@yahoo.co.jp>
2020-05-28 20:12:15 +0900

    Reduce code

--- 07_cache_gpu/cutlass/gemm.cu
+++ 07_cache_gpu/cutlass/gemm.cu
@@ -13,69 +13,32 @@
 using namespace cutlass;
 
 int main(int argc, const char **argv) {
-  int m = 10240;
-  int k = 4096;
-  int n = 4096;
-  float alpha = 1.0;
-  float beta = 0.0;
-  static const matrix_transform_t::kind_t TransformA = matrix_transform_t::NonTranspose;
-  static const matrix_transform_t::kind_t TransformB = matrix_transform_t::NonTranspose;
-  typedef float value_t;
-  typedef float accum_t;
-  int g_timing_iterations = 10;
+  int m = 10240, k = 4096, n = 4096, g_timing_iterations = 10;
+  float alpha = 1.0, beta = 0.0;
   cudaStream_t stream = 0;
-  matrix<value_t> A(m, k);
-  matrix<value_t> B(k, n);
-  matrix<accum_t> C(m, n);
-  matrix<accum_t> C2(m, n);
+  matrix<float> A(m, k), B(k, n), C(m, n), C2(m, n);
   A.random();
   B.random();
-  C.fill_ramp(0,0);
-  C2.fill_ramp(0,0);
   A.sync_device();
   B.sync_device();
-  C.sync_device();
-  C2.sync_device();
   cublasHandle_t g_cublas_handle;
   cublasCreate(&g_cublas_handle);
   gpu_timer timer;
   for (int i = 0; i < g_timing_iterations+2; i++) {
     if (i == 2) timer.start();
-    CUDA_PERROR(cublasSgemm(
-                            g_cublas_handle,
-                            (cublasOperation_t) TransformA,
-                            (cublasOperation_t) TransformB,
-                            m,
-                            n,
-                            k,
-                            &alpha,
-                            A.d_data(),
-                            m,
-                            B.d_data(),
-                            k,
-                            &beta,
-                            C.d_data(),
-                            m));
+    cublasSgemm(g_cublas_handle,
+        (cublasOperation_t)matrix_transform_t::NonTranspose, (cublasOperation_t)matrix_transform_t::NonTranspose,
+        m, n, k, &alpha, A.d_data(), m, B.d_data(), k, &beta, C.d_data(), m);
   }
   timer.stop();
   int64_t num_flops = (2 * int64_t(m) * int64_t(n) * int64_t(k)) + (2 * int64_t(m) * int64_t(n));
   double tcublas = timer.elapsed_millis() / g_timing_iterations;
   double cublas_flops = double(num_flops) / tcublas / 1.0e6;
-  typedef gemm::blas_scaled_epilogue<float, float, float> epilogue_op_t;
-  epilogue_op_t epilogue(alpha, beta);
-  for (int i = 0; i < g_timing_iterations+2; i++) {
-    if (i == 2) timer.start();
-    gemm::dispatch<epilogue_op_t>(
-        m,
-        n,
-        k,
-        alpha,
-        beta,
-        A.d_data(),
-        B.d_data(),
-        C2.d_data(),
-        stream,
-        false);
+  timer.start();
+  for (int i = 0; i < g_timing_iterations/*+2*/; i++) {
+    // if (i == 2) timer.start();
+    gemm::dispatch<gemm::blas_scaled_epilogue<float, float, float>>(
+        m, n, k, alpha, beta, A.d_data(), B.d_data(), C2.d_data(), stream, false);
   }
   timer.stop();
   double tcutlass = timer.elapsed_millis() / g_timing_iterations;
@@ -84,11 +47,9 @@ int main(int argc, const char **argv) {
   C.sync_host();
   C2.sync_host();
   double err = 0;
-  for (int i=0; i<n; i++) {
-    for (int j=0; j<m; j++) {
+  for (int i=0; i<n; i++)
+    for (int j=0; j<m; j++)
       err += fabs(C.get(i,j) - C2.get(i,j));
-    }
-  }
   printf("error: %lf\n", err/n/m);
   cublasDestroy(g_cublas_handle);
 }
